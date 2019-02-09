715c9c736b717ffa0f342eac9f2821604099e273
david-knigge <davidknigge@live.nl>
2019-02-09 16:49:16 +0100

    first commit

---
+++ app/src/main/java/nl/uva/student/davidknigge/nas19_tictactoe/MainActivity.java
@@ -0,0 +1,13 @@
+package nl.uva.student.davidknigge.nas19_tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
