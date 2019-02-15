3af3f26ef25681dd7de95477b7c5fef2fdb76ed4
david-knigge <davidknigge@live.nl>
2019-02-15 10:28:17 +0100

    landscape mode

--- app/src/main/java/nl/uva/student/davidknigge/nas19_tictactoe/Game.java
+++ app/src/main/java/nl/uva/student/davidknigge/nas19_tictactoe/Game.java
@@ -18,6 +18,9 @@ public class Game implements Serializable{
     private static Mode defaultMode = Mode.MULTI_PLAYER;
     private Mode mode;
 
+    /*
+     * Initialize board and gamemode. Set turn to player one.
+     */
     public Game(Mode gameMode) {
         board = new TileState[BOARD_SIZE][BOARD_SIZE];
         for(int i=0; i<BOARD_SIZE; i++)
@@ -29,10 +32,17 @@ public class Game implements Serializable{
         mode = gameMode;
     }
 
+    /*
+     * If game is constructed without gamemode, set defaultMode as gamemode.
+     */
     public Game() {
         this(defaultMode);
     }
 
+    /*
+     * Checks if given index is blank, if not, set value of index according to players turn.
+     * Returns invalid if tile is occupied, the set tile value otherwise.
+     */
     public TileState choose(int row, int column) {
         TileState tile = TileState.INVALID;
         if (!gameOver && board[row][column] == TileState.BLANK) {
@@ -48,14 +58,23 @@ public class Game implements Serializable{
         return tile;
     }
 
+    /*
+     * Get the size of the board.
+     */
     public int getBoardSize() {
         return BOARD_SIZE;
     }
 
+    /*
+     * Get tile at given index.
+     */
     public TileState getTileAt(int row, int column) {
         return board[row][column];
     }
 
+    /*
+     * Get player whose turn it is.
+     */
     public GameState getTurn() {
         if (gameOver) return GameState.DRAW;
         if (playerOneTurn) {
@@ -65,15 +84,20 @@ public class Game implements Serializable{
         }
     }
 
+    /*
+     * Get state of the game, checks if any player has won, or if game is a draw. If the game is won
+     * return GameState of winning player, return GameState.draw if draw and GameState.IN_PROGRESS
+     * if game is still in progress.
+     */
     public GameState getGameState() {
         for (int row=0; row<BOARD_SIZE; row++) {
-            gameOver = gameOver | (board[row][0] == board[row][1] && board[row][0] == board[row][2] && board[row][0] != TileState.BLANK);
+            gameOver = gameOver || (board[row][0] == board[row][1] && board[row][0] == board[row][2] && board[row][0] != TileState.BLANK);
         }
         for (int col=0; col<BOARD_SIZE; col++) {
-            gameOver = gameOver | (board[0][col] == board[1][col] && board[0][col] == board[2][col] && board[0][col] != TileState.BLANK);
+            gameOver = gameOver || (board[0][col] == board[1][col] && board[0][col] == board[2][col] && board[0][col] != TileState.BLANK);
         }
-        gameOver = gameOver | (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != TileState.BLANK);
-        gameOver = gameOver | (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != TileState.BLANK);
+        gameOver = gameOver || (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != TileState.BLANK);
+        gameOver = gameOver || (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != TileState.BLANK);
 
         if (gameOver) {
             if (playerOneTurn) {
@@ -89,6 +113,9 @@ public class Game implements Serializable{
         }
     }
 
+    /*
+     * Gets the index of a random vacant tile.
+     */
     public int getComputerMove() {
         Random rand = new Random();
         int tileIndex, row, col;
@@ -104,10 +131,16 @@ public class Game implements Serializable{
         return tileIndex;
     }
 
-    public boolean gameOver() {
+    /*
+     * Returns whether game is over.
+     */
+    public boolean getGameOver() {
         return gameOver;
     }
 
+    /*
+     * Return gamemode.
+     */
     public Mode getMode() {
         return mode;
     }
--- app/src/main/java/nl/uva/student/davidknigge/nas19_tictactoe/MainActivity.java
+++ app/src/main/java/nl/uva/student/davidknigge/nas19_tictactoe/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     Mode mode;
     Menu optionsMenu;
 
+    /*
+     * If bundle is initialized, sets the saved gamestate. Sets tiles appropriately, check if game
+     * is over.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,22 +36,33 @@ public class MainActivity extends AppCompatActivity {
         mode = game.getMode();
         setTiles();
         checkGameState();
-        setModeIcon();
     }
 
+    /*
+     * Save the Game.
+     */
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putSerializable("game", game);
     }
 
+    /*
+     * Set the options menu on this activity.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.top_menu, menu);
         optionsMenu = menu;
+
+        setModeIcon();
         return true;
     }
 
+    /*
+     * Option buttons listener. If the gamemode button is pressed change the icon and reset the game
+     * if reset is clicked, reset the game.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -61,6 +76,11 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /*
+     * Tile click listener, obtain row and column number. If tile on coordinates is empty, set it
+     * according to which player's turn it is. If Game is in Mode.SINGLE_PLAYER let computer make
+     * a move.
+     */
     public void tileClicked(View view) {
 
         Button button = (Button) view;
@@ -83,18 +103,34 @@ public class MainActivity extends AppCompatActivity {
         }
         checkGameState();
 
-        if (!game.gameOver() && mode == Mode.SINGLE_PLAYER && game.getTurn() == GameState.PLAYER_TWO_TURN) {
+        if (!game.getGameOver() && mode == Mode.SINGLE_PLAYER && game.getTurn() == GameState.PLAYER_TWO_TURN) {
             computerClick();
             checkGameState();
         }
     }
 
+    /*
+     * Simulate computer clicking on tile.
+     */
+    public void computerClick() {
+        int tileIndex = game.getComputerMove();
+        ViewGroup gameContainer = findViewById(R.id.game_container);
+        View button = gameContainer.getChildAt(tileIndex);
+        button.performClick();
+    }
+
+    /*
+     * Reset the game.
+     */
     public void resetClicked() {
         game = new Game(mode);
         setTiles();
         checkGameState();
     }
 
+    /*
+     * Set tiles according to Gameboard's values.
+     */
     public void setTiles() {
         ViewGroup gameContainer = findViewById(R.id.game_container);
 
@@ -118,6 +154,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+     * Obtain the GameState, change tiles according.
+     */
     public void checkGameState() {
         GameState state = game.getGameState();
         TextView stateAnnouncement = findViewById(R.id.textViewGameState);
@@ -142,6 +181,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+     * Toggle gamemode.
+     */
     public Mode toggleMode() {
         if (mode == Mode.MULTI_PLAYER) {
             mode = Mode.SINGLE_PLAYER;
@@ -151,18 +193,16 @@ public class MainActivity extends AppCompatActivity {
         return mode;
     }
 
-    public void computerClick() {
-        int tileIndex = game.getComputerMove();
-        ViewGroup gameContainer = findViewById(R.id.game_container);
-        View button = gameContainer.getChildAt(tileIndex);
-        button.performClick();
-    }
-
+    /*
+     * Set the correct gamemode icon.
+     */
     public void setModeIcon() {
-        if (mode == Mode.MULTI_PLAYER) {
-            optionsMenu.findItem(R.id.modeButton).setIcon(R.drawable.ic_twop);
-        } else {
-            optionsMenu.findItem(R.id.modeButton).setIcon(R.drawable.ic_onep);
+        if (optionsMenu != null) {
+            if (mode == Mode.MULTI_PLAYER) {
+                optionsMenu.findItem(R.id.modeButton).setIcon(R.drawable.ic_twop);
+            } else {
+                optionsMenu.findItem(R.id.modeButton).setIcon(R.drawable.ic_onep);
+            }
         }
     }
 }
